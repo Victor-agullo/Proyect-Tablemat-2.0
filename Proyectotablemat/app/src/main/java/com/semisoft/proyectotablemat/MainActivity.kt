ade989f6b27f73fd71bcc322b1c5fbbd220f5a3f
Víctor <38455769+Victor-agullo@users.noreply.github.com>
2020-12-29 19:22:32 +0100

    initial commit

---
+++ Proyectotablemat/app/src/main/java/com/semisoft/proyectotablemat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.semisoft.proyectotablemat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
